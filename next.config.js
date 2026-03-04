66de1267562218c6f67efc44c96313d65426e518
LeonXY <ev5533@126.com>
2026-03-04 18:32:17 +0800

    Modified build script to run convex codegen

--- next.config.js
+++ next.config.js
@@ -12,11 +12,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Ignore TypeScript errors in production builds
     ignoreBuildErrors: true,
